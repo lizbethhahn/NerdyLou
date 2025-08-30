8f16114bce7fcea659b8b657198429ace393e140
lizbethhahn <lizbethhahn@gmail.com>
2025-08-30 10:45:32 -0400

    Added MudBlazor. Not perfect but working. Utilized Shared Layout, Nav

--- NerdyLou.MAUI/MauiProgram.cs
+++ NerdyLou.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace NerdyLou.MAUI;
 
@@ -15,9 +16,10 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMudServices();
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 
